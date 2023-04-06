0199e20268905b4f6489d1ba526b4eef5072f569
Hivludi <iatskov.v@yandex.ru>
2023-04-07 01:19:20 +0300

    refactor: Использован lombok data, приватные конструкторы мапперов, sql запросы корректно отформатированы, пустые конструкторы где необходимо заменены на аннотации

--- src/main/java/ru/practicum/shareit/comment/dto/CommentCreateDto.java
+++ src/main/java/ru/practicum/shareit/comment/dto/CommentCreateDto.java
@@ -1,14 +1,12 @@
 package ru.practicum.shareit.comment.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@AllArgsConstructor
-@Builder(toBuilder = true)
+@NoArgsConstructor
 public class CommentCreateDto {
     @NotBlank
     private String text;
